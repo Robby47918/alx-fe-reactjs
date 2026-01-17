62e6c7dfd2654d57b0a80602b1b320b2416e258a
Roberta Smith <efyarobbi@gmail.com>
2026-01-17 12:19:23 +0000

    Add user context and profile page components

---
+++ alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main>
+             <p style={{ padding: "20px", color: "black" }}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
