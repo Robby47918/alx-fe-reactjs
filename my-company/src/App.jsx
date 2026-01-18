7b4f6d2fd593a18f83fe6161dd2589cacf0fa11a
Roberta Smith <efyarobbi@gmail.com>
2026-01-19 02:16:50 +0800

    Add the files to src and create a new components

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,13 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
+import Navbar from "./components/Navbar"; 
+import Home from "./Home"; 
+import About from "./About"; 
+import Services from "./Services"; 
+import Contact from "./Contact";
+import Footer from "./components/Footer";
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from "./components/Footer";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +45,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </Router>
   )
 }
 
---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+function Footer() { 
+    return ( <footer style={{ backgroundColor: "#eee", padding: "10px", textAlign: "center" }}> 
+    <p>© 2026 My Company. All rights reserved.</p> 
+    </footer> 
+    ); 
+
+} export default Footer;
