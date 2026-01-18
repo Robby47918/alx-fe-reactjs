021e6743d3a3650aabc1705913b3d3f5b93d0b88
Roberta Smith <efyarobbi@gmail.com>
2026-01-19 02:29:54 +0800

    Delet my company/package.json

--- my-company/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-function Footer() { 
-    return ( <footer style={{ backgroundColor: "#eee", padding: "10px", textAlign: "center" }}> 
-    <p>© 2026 My Company. All rights reserved.</p> 
-    </footer> 
-    ); 
-
-} export default Footer;
