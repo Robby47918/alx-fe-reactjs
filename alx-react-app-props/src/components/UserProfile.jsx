a7778d9ba6f6e6f747cc1befb6e3f86e23f33f0d
Roberta Smith <efyarobbi@gmail.com>
2026-01-19 01:39:17 +0800

    Add user context with user data

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from "react";
+import UserContext from "/UserContext";
 
 const UserProfile = (props) => {
   return (
-    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-      <h2 style={{ color: 'blue' }}>{props.name}</h2>
-      <p >Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span>{props.age}</p>
+    <div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
+      <h2 style={{ color: "blue" }}>{props.name}</h2>
+      <p>
+        Age: <span style={{ fontWeight: "bold" }}>{props.age}</span>
+        {props.age}
+      </p>
       <p>Bio: {props.bio}</p>
     </div>
   );
-}
+};
 
 export default UserProfile;
