53320b98b10da42f4b6a16c68ae6b63270cdd075
Roberta Smith <efyarobbi@gmail.com>
2026-01-25 01:43:23 +0800

    Add code to recipeStore.js to handle adding new recipes to the store.

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -5,15 +5,20 @@ const useRecipeStore = create(set => ({
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes }),
 
-  searchTerm: '',
-  setSearchTerm: (term) => set({ searchTerm: term }),
-  filteredRecipes: [],
-  filterRecipes: () => set(state => ({
-    filteredRecipes: state.recipes.filter(recipe =>
-      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
-    )
-  }))
-}));
+  updateRecipe: (id, updates) => 
+    set((state) => ({ 
+      recipes: state.recipes.map((r) => 
+        r.id === id ? { ...r, ...updates } : r 
+    ), 
+  })), 
+  deleteRecipe: (id) => 
+    set((state) => ({ 
+      recipes: state.recipes.filter((r) => r.id !== id), 
+    })), 
+  }));
+
+ 
+
 
 
 export default useRecipeStore;
