a3111137029b53a71aea413e36661a1defc57eb3
Roberta Smith <efyarobbi@gmail.com>
2026-01-25 01:36:52 +0800

    Add recipe form and recipe list components to App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,6 @@
 import AddRecipeForm from './components/AddRecipeForm'
 import RecipeList from './components/RecipeList'
+import RecipeDetails from './components/RecipeDetails'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
