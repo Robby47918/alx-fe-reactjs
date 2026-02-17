8acba9de39d850fa1bcf3489891ea8e6b9f5904e
Roberta Smith <efyarobbi@gmail.com>
2026-02-17 21:24:14 +0000

    Updated the ProtectedRoute.jsx file

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,7 +1,11 @@
 import { Navigate } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
 
-function ProtectedRoute({ isAuth, children }) {
+function ProtectedRoute({ children }) {
+  const { isAuth } = useAuth();
+
   return isAuth ? children : <Navigate to="/login" />;
 }
 
 export default ProtectedRoute;
+
